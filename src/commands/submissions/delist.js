d2b70f51155aa2bc86db9c92299466aff3781663
bndh <archiebundy15@gmail.com>
2024-11-09 15:08:57 +0000

    Delist by Id Introducted

--- src/commands/submissions/delist.js
+++ src/commands/submissions/delist.js
@@ -5,19 +5,43 @@ const { Judge } = require("../../mongo/mongoModels").modelData;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("delist")
-		.setDescription("Manually remove a user from their position.")
-		.addUserOption(optionBuilder => 
-			optionBuilder.setName("delistee")
-				.setDescription("The person to be delisted.")
+		.setDescription("Manually remove a user from their judge position.")
+		.addSubcommand(subCommandBuilder => subCommandBuilder
+			.setName("by-tag")
+			.setDescription("Delist a judge via their handle. (Recommended while they are in the server).")
+			.addUserOption(optionBuilder => optionBuilder
+				.setName("delistee")
+				.setDescription("The user to be delisted.")
 				.setRequired(true)
+			)
+		)
+		.addSubcommand(subCommandBuilder => subCommandBuilder
+			.setName("by-id")
+			.setDescription("Delist a judge via their id. (Recommended if they are not in the server).")
+			.addStringOption(optionBuilder => optionBuilder
+				.setName("delistee-id")
+				.setDescription("The user to be delisted's account id.")
+				.setRequired(true)
+			)
 		)
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
 		await interaction.deferReply({ephemeral: true});
 
-		const delistee = interaction.options.getUser("delistee", true);
+		let delistee, delisteeId;
+		const subcommandId = interaction.options.getSubcommand()
+		if(subcommandId === "by-tag") {
+			delistee = interaction.options.getUser("delistee", true);
+			delisteeId = delistee.id;
+		} else if(subcommandId === "by-id") {
+			delisteeId = interaction.options.getString("delistee-id", true);
+		} else {
+			await interaction.editReply({embeds: [EmbedBuilder.generateFailEmbed()]});
+		}
 
-		await Judge.enqueue(() => Judge.deleteOne({userId: delistee.id}).exec());
-		await interaction.editReply({embeds: [EmbedBuilder.generateSuccessEmbed(`Successfully delisted ${delistee.toString()}!`)]});
+		await Judge.enqueue(() => Judge.deleteOne({userId: delisteeId}).exec());
+		await interaction.editReply({embeds: [
+			EmbedBuilder.generateSuccessEmbed(`Successfully delisted ${delistee ? delistee.toString() : `<@${delisteeId}>`}!`)
+		]});
 	}
 }; 
