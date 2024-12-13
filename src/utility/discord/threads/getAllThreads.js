9c4f34928f0cfae93ad80297308729923c22cfb5
Archie Bundy <archiebundy15@gmail.com>
2024-12-13 17:01:22 +0000

    Auto-unarchive Removed (Archive Cap Exists)

--- src/utility/discord/threads/getAllThreads.js
+++ src/utility/discord/threads/getAllThreads.js
@@ -1,17 +1,11 @@
 const getActiveThreads = require("./getActiveThreads");
 const getArchivedThreads = require("./getArchivedThreads");
 
-module.exports = async (targetForum, autoUnarchive = false) => {
+module.exports = async (targetForum) => {
 	const threads = await Promise.all([
 		getActiveThreads(targetForum), 
 		getArchivedThreads(targetForum)
 	]);
-	
-	if(autoUnarchive) {
-		await Promise.all(threads[1].map(thread => {
-			if(thread.archived === true) return thread.setArchived(false);
-		})); // Unarchive all archived threads because important information cannot be accessed when archived
-	}
 
 	return threads[0].concat(threads[1]);
 }
